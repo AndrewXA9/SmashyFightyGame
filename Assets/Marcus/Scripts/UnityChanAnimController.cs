5afba50e900e0e20468bc45370040496671cff70
marcusmriley <marcusmriley@gmail.com>
2015-04-20 12:44:08 -0700

    UnityChan walking and push for movement testing

---
+++ Assets/Marcus/Scripts/UnityChanAnimController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanAnimController : MonoBehaviour 
+{
+
+	private Animator anim;
+	float movement;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		movement = Input.GetAxis("Horizontal");
+		anim.SetFloat("movement",movement);
+	}
+}
