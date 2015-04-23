c071ce9f2d4de87842a497a0ad9ad3521d8bb265
marcusmriley <marcusmriley@gmail.com>
2015-04-23 00:20:35 -0700

    refactoring and bug smashing

--- Assets/Marcus/Scripts/MultiPlayerCameraFocus.cs
+++ Assets/Marcus/Scripts/MultiPlayerCameraFocus.cs
@@ -12,14 +12,15 @@ using System.Collections.Generic;
 public class MultiPlayerCameraFocus : MonoBehaviour
 {
 
-	[SerializeField] private List<Transform> players;		// Grab the transforms of all the players from inspector
+	[SerializeField] private List<Transform> players;	// Grab the transforms of all the players from inspector
 	[SerializeField] private Vector3 cameraBuffer;		// Alter the home position of the camera
 	[SerializeField] private Vector2 minCameraPos;		// Minimum camera x and y position
 	[SerializeField] private Vector2 maxCameraPos;		// Maximum camera x and y postion
-	[SerializeField] private float cameraZoomScale;		// How fast the camera zooms in and out
+	[SerializeField] private float cameraFollowSpeed;	// How fast the camera zooms in and out
 
-	private List<float> m_xPositions = new List<float>();
-	private List<float> m_yPositions = new List<float>();
+	private enum cameraProjection{Perspective, Orthographic};
+	private List<float> xPositions = new List<float>();
+	private List<float> yPositions = new List<float>();
 	private float maxX;
 	private float maxY;
 	private float minX;
@@ -29,32 +30,25 @@ public class MultiPlayerCameraFocus : MonoBehaviour
 	private Vector3 centerPosition;
 	private Vector3 zoomValue;
 	private Vector3 cameraPosition;
-
+	[SerializeField] private cameraProjection cameraMode;
 
 
 	// Update is called once per frame
 	void Update () 
 	{
 		positionCamera();
+		Debug.Log(zoomValue);
 	}
 
 	void positionCamera()
 	{
-		foreach(Transform player in players)
-		{
-			m_xPositions.Add(player.transform.position.x);
-			m_yPositions.Add(player.transform.position.y);
-		}
-		
-		//setting the min and max player positions, check if the positions pass the min and max vectors
-		maxX = (Mathf.Max(m_xPositions.ToArray()) > maxCameraPos.x) ? maxCameraPos.x : Mathf.Max(m_xPositions.ToArray());
-		maxY = (Mathf.Max(m_yPositions.ToArray()) > maxCameraPos.y) ? maxCameraPos.y : Mathf.Max(m_yPositions.ToArray());
-		minX = (Mathf.Min(m_xPositions.ToArray()) < minCameraPos.x) ? minCameraPos.x : Mathf.Min(m_xPositions.ToArray());
-		minY = (Mathf.Min(m_yPositions.ToArray()) < minCameraPos.y) ? minCameraPos.y : Mathf.Min(m_yPositions.ToArray());
+		getPlayerPositions();
+
+		setMinMaxPlayerPositions();
 		
 		//clear lists for next update
-		m_xPositions.Clear();
-		m_yPositions.Clear();
+		xPositions.Clear();
+		yPositions.Clear();
 		
 		//finding the center follow position for the camera
 		maxPosition = new Vector3(maxX,maxY,0);
@@ -78,7 +72,24 @@ public class MultiPlayerCameraFocus : MonoBehaviour
 		
 		
 		//Final Camera Position with Buffer for start position
-//		transform.position = cameraPosition + cameraBuffer;
-		transform.position = Vector3.Lerp(transform.position, (cameraPosition + cameraBuffer),Time.deltaTime * cameraZoomScale);
+		transform.position = Vector3.Lerp(transform.position, (cameraPosition + cameraBuffer),Time.deltaTime * cameraFollowSpeed);
+	}
+
+
+	void getPlayerPositions()
+	{
+		foreach(Transform player in players)
+		{
+			xPositions.Add(player.transform.position.x);
+			yPositions.Add(player.transform.position.y);
+		}
+	}
+
+	void setMinMaxPlayerPositions()
+	{
+		maxX = (Mathf.Max(xPositions.ToArray()) > maxCameraPos.x) ? maxCameraPos.x : Mathf.Max(xPositions.ToArray());
+		maxY = (Mathf.Max(yPositions.ToArray()) > maxCameraPos.y) ? maxCameraPos.y : Mathf.Max(yPositions.ToArray());
+		minX = (Mathf.Min(xPositions.ToArray()) < minCameraPos.x) ? minCameraPos.x : Mathf.Min(xPositions.ToArray());
+		minY = (Mathf.Min(yPositions.ToArray()) < minCameraPos.y) ? minCameraPos.y : Mathf.Min(yPositions.ToArray());
 	}
 }
