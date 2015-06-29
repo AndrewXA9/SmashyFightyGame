cddd6baec12a281fd494a374a4e7e330880ff4f1
Andrew <andrewxa9@yahoo.com>
2015-06-28 21:01:04 -0700

    Movement intermediary script

---
+++ Assets/Skills/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement{
+
+	private static float[] Hmove = new float[4]{0f,0f,0f,0f};
+	private static float[] Vmove = new float[4]{0f,0f,0f,0f};
+	
+	
+	public static void UpdateHorizontal(float value, int player){
+		Hmove[player] = Mathf.Clamp(value,-1f,1f);
+	}
+	
+	public static void UpdateVertical(float value, int player){
+		Vmove[player] = Mathf.Clamp(value,-1f,1f);
+	}
+	
+	public static float GetHorizontal(int player){
+		return Hmove[player];
+	}
+	public static float GetVertical(int player){
+		return Vmove[player];
+	}
+	
+}
